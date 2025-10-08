a7a56e9be05dfde51edbddc7dcdebdb7052e6c81
Надежда Козулина <nadine.kozulina@gmail.com>
2025-10-08 15:48:18 +0500

    исправление кода

--- js/main.js
+++ js/main.js
@@ -107,16 +107,13 @@ function generatePhotos(num = 25) {
   for (let i = 0; i < num; i++) {
 
     const description = descriptions[i % descriptions.length];
-
     const likes = getRandomInt(15, 200);
-
     const commentsCount = getRandomInt(0, 30);
-
     const comments = Array.from({ length: commentsCount }, generateComment);
 
     photos.push({
       id: photoIds[i],
-      url: `photos/${i + 1}.jpg`,
+      url: `photos/${photoIds[i]}.jpg`,
       description,
       likes,
       comments
